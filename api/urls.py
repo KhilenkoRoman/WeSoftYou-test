582b3fc3a138d37b53fb5b0b5ee852b8eeb1bcfc
Роман Хиленко <KhilenkoRoman@gmail.com>
2021-11-23 10:43:26 +0000

    add lazy

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
-from .views import CarView, CarGetView, CarAggregationView
+from .views import CarView, CarGetView, CarAggregationView, LazyTestView
 
 
 app_name = 'api'
 urlpatterns = [
+    path('lazy_test/', LazyTestView.as_view(), name='lazy_test'),
     path('cars/', CarView.as_view(), name='cars'),
     path('get_cars/', CarGetView.as_view(), name='get_cars'),
     path('aggregate_cars/', CarAggregationView.as_view(), name='aggregate_cars'),
--- api/views.py
+++ api/views.py
@@ -7,6 +7,17 @@ from .filters import CarFilter
 from django_filters import rest_framework as filters
 from django.db.models import Sum
 
+
+class LazyTestView(generics.ListAPIView):
+
+    def get(self, request, *args, **kwargs):
+        cars = Car.objects.all()
+        cars = cars.filter(name="car name 0")
+        cars = cars.order_by('price')
+        car = cars.first()
+        return Response(f"price = {car.price}", status=status.HTTP_200_OK)
+
+
 class CarView(generics.ListAPIView):
     serializer_class = CarSerializer
     queryset = Car.objects.all()
